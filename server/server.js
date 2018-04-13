401bd71bfc09b6773ea2fe456929920ea4078d98
vn0f7by <jiayi.he@wartmart.com>
2018-04-13 16:03:53 -0500

    send message to the right room

--- server/server.js
+++ server/server.js
@@ -44,7 +44,11 @@ io.on('connection',(socket) => {
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessate',message);
-        io.emit('newMessage',generateMes(message.from,message.text))
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text))
+        {
+        io.to(user.room).emit('newMessage',generateMes(user.name,message.text))
+        }
         callback();
         // socket.broadcast.emit('newMessage',{
         //     from:message.from,
@@ -54,7 +58,9 @@ io.on('connection',(socket) => {
     })
 
     socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMes('Admin',coords.latitude,coords.longitude))
+        var user = users.getUser(socket.id);
+        if(user)
+        io.to(user.room).emit('newLocationMessage',generateLocationMes(user.name,coords.latitude,coords.longitude))
     });
 
 
